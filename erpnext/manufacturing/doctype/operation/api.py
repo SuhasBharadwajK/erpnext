a96943b4a06b49cca71a17aa80f65ce9421fb22f
Suhas Bharadwaj <suhas.five@gmail.com>
2026-06-25 09:51:18 +0530

    fix: update save point implementation

--- erpnext/manufacturing/doctype/operation/api.py
+++ erpnext/manufacturing/doctype/operation/api.py
@@ -376,16 +376,15 @@ def create_material_transfer_stock_entry(
 	stock_entry.set_stock_entry_type()
 	stock_entry.set_missing_values()
 
-	# frappe.db.savepoint("stock_entry_checkpoint")
-
 	for i in range(10):
 		try:
+			frappe.db.savepoint("stock_entry_checkpoint")
 			stock_entry.insert()
 			stock_entry.submit()
 			break
 
 		except QueryDeadlockError:
-			# frappe.db.rollback(save_point="stock_entry_checkpoint")
+			frappe.db.rollback(save_point="stock_entry_checkpoint")
 			if i <= 9:
 				time.sleep(0.5)
 				continue
@@ -393,8 +392,7 @@ def create_material_transfer_stock_entry(
 			raise
 
 		finally:
-			pass
-			# frappe.db.release_savepoint("stock_entry_checkpoint")
+			frappe.db.release_savepoint("stock_entry_checkpoint")
 
 	return stock_entry
 
--- erpnext/manufacturing/page/operator_station/operator_station.py
+++ erpnext/manufacturing/page/operator_station/operator_station.py
@@ -262,16 +262,15 @@ def finish_process(
 	stock_entry_manufacture.naming_series = STOCK_ENTRY_NAMING_SERIES_MAP.get(process_name.lower(), "MAT-STE-.YYYY.-")  # pyright: ignore[reportAttributeAccessIssue]
 	stock_entry_manufacture.fg_completed_qty = job_card_qty
 
-	# frappe.db.savepoint("mfg_stock_entry_checkpoint")
-
 	for i in range(10):
 		try:
+			frappe.db.savepoint("mfg_stock_entry_checkpoint")
 			stock_entry_manufacture.insert()
 			stock_entry_manufacture.submit()
 			break
 
 		except QueryDeadlockError:
-			# frappe.db.rollback(save_point="mfg_stock_entry_checkpoint")
+			frappe.db.rollback(save_point="mfg_stock_entry_checkpoint")
 			if i < 9:
 				time.sleep(0.5)
 				continue
@@ -279,8 +278,7 @@ def finish_process(
 			raise
 
 		finally:
-			pass
-			# frappe.db.release_savepoint("mfg_stock_entry_checkpoint")
+			frappe.db.release_savepoint("mfg_stock_entry_checkpoint")
 
 	wo.update_work_order_qty()
 	wo.reload()
